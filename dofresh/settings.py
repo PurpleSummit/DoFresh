82ef5ede502a651a4b4d1b204545b94549952227
PurpleSummit <yaksok.world@gmail.com>
2026-09-16 22:24:42 -0400

    Added dofresh.onrender.com to allowed_hosts

--- dofresh/settings.py
+++ dofresh/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rx$afx-%lyb=1@0yq0wuh&8r#+_u-!$t9u8*2i$ea*v*!2a79!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dofresh.onrender.com"]
 
 
 # Application definition
